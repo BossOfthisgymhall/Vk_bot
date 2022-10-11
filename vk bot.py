5c59facabf51834016bc97e85be3f49cb7d03900
Ivan <vanya0150396@gmail.com>
2022-10-11 20:16:33 +0300

    Update vk bot.py

--- vk bot.py
+++ vk bot.py
@@ -1,7 +1,8 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = "vk1.a.xI6OrHXJE2d-ctlqxZpsYYm5wDTvVlEi-zxHoGkAXD7_MPY04jZscnnaQDWZScyMJhWhMfNQwsxM6zSmRJLi1gimvq29h_GCSF_tThTly6uJFDKFUUBDV758C1WOEYZ3oRjU0YIwT-H_Tw2XodojBW-Qe58_tkWGU0DfCNbTPSty8pOu5C_Y-Sk-Z6m3tFy5"
+
+token = 'vk1.a.xI6OrHXJE2d-ctlqxZpsYYm5wDTvVlEi-zxHoGkAXD7_MPY04jZscnnaQDWZScyMJhWhMfNQwsxM6zSmRJLi1gimvq29h_GCSF_tThTly6uJFDKFUUBDV758C1WOEYZ3oRjU0YIwT-H_Tw2XodojBW-Qe58_tkWGU0DfCNbTPSty8pOu5C_Y-Sk-Z6m3tFy5'
 session = vk_api.VkApi(token=token)
 
 def send_message(user_id, message):
