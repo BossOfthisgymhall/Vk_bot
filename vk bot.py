2d24f238524ac3cbbb2223d0b11565a71a898f7d
Ivan <vanya0150396@gmail.com>
2022-10-11 21:10:57 +0300

    Update vk bot.py

--- vk bot.py
+++ vk bot.py
@@ -1,20 +1,25 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+TOKEN = "vk1.a.xI6OrHXJE2d-ctlqxZpsYYm5wDTvVlEi-zxHoGkAXD7_MPY04jZscnnaQDWZScyMJhWhMfNQwsxM6zSmRJLi1gimvq29h_GCSF_tThTly6uJFDKFUUBDV758C1WOEYZ3oRjU0YIwT-H_Tw2XodojBW-Qe58_tkWGU0DfCNbTPSty8pOu5C_Y-Sk-Z6m3tFy5"
 
-token = 'vk1.a.xI6OrHXJE2d-ctlqxZpsYYm5wDTvVlEi-zxHoGkAXD7_MPY04jZscnnaQDWZScyMJhWhMfNQwsxM6zSmRJLi1gimvq29h_GCSF_tThTly6uJFDKFUUBDV758C1WOEYZ3oRjU0YIwT-H_Tw2XodojBW-Qe58_tkWGU0DfCNbTPSty8pOu5C_Y-Sk-Z6m3tFy5'
-session = vk_api.VkApi(token=token)
+session = vk_api.VkApi(token=TOKEN)
+
 
 def send_message(user_id, message):
-    session.method("messages.send", {
-        "user_id": user_id,
-        "message": message,
-        "random_id": 0
-    })
-for event in VkLongPoll(session).listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        text = event.text.lower()
-        user_id = event.user_id
+	session.method("messages.send",
+		{
+			"user_id": user_id,
+			"message": message,
+			"random_id": 0
+		}
+	)
 
-        if text == 'привет':
-            send_message(user_id, "Привет чепуха!")
+
+for event in VkLongPoll(session).listen():
+	if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+		text = event.text.lower()
+		user_id = event.user_id
+
+		if text == "hello":
+			send_message(user_id, "Hello!")
